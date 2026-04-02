4dcfb6d16841879c3fa110094cf55eb79aa7d77a
Oleg <oleg.dudar@gmail.com>
2026-04-02 21:58:59 +0200

    Modify query parameters and date format in build.py
    Updated query parameters and output formatting in build.py.

--- build.py
+++ build.py
@@ -8,14 +8,14 @@ url = f"{os.getenv('SUPABASE_URL')}/rest/v1/products"
 headers = {
     "apikey": os.getenv('SUPABASE_KEY'),
     "Authorization": f"Bearer {os.getenv('SUPABASE_KEY')}",
-    "Prefer": "return=representation"
 }
 params = {
     "store": "eq.mattel",
     "is_active": "eq.true",
     "current_qty": "lt.625",
+    "current_qty": "gt.0",  # ВІДСІЮЄМО НУЛІ
     "select": "title,image,url,current_qty",
-    "order": "current_qty.asc"
+    "order": "current_qty.asc" # За замовчуванням від мін до макс
 }
 
 response = requests.get(url, headers=headers, params=params)
@@ -27,9 +27,8 @@ with open('template.html', 'r') as f:
 
 html_output = template.render(
     products=products, 
-    last_updated=datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    last_updated=datetime.now().strftime("%d.%m.%Y %H:%M")
 )
 
-# 3. Запис результату
 with open('index.html', 'w') as f:
     f.write(html_output)
